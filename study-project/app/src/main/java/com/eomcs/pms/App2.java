eb5755efe0a274039e33a886e102b23649e39358
baekyeon9 <dev.baekyeon@gmail.com>
2021-07-05 20:57:00 +0900

    .

---
+++ study-project/app/src/main/java/com/eomcs/pms/App2.java
@@ -0,0 +1,24 @@
+package com.eomcs.pms;
+
+public class App2 {
+
+  public static void main(String[] args) {
+    System.out.println("[작업]");
+    System.out.println("프로젝트 : 미니 프로젝트 관리 시스템 개발");
+    System.out.println("번호 : 1");
+    System.out.println("내용 : 요구사항 수집");
+    System.out.println("완료일 : 2020-01-20");
+    System.out.println("상태 : 진행중");
+    System.out.println("담당자 : 홍길동");
+
+    System.out.printf("[%s]\n", "작업");
+    System.out.printf("프로젝트: %s\n", "미니 프로젝트 관리 시스템 개발");
+    System.out.printf("번호 : %d\n", 1);
+    System.out.printf("내용: %s\n", "요구사항 수집");
+    System.out.printf("완료일 : %s\n", "2021-01-20");
+    System.out.printf("상태 : %s\n", "진행중");
+    System.out.printf("담당자 : %s\n", "홍길동");
+
+  }
+}
+
