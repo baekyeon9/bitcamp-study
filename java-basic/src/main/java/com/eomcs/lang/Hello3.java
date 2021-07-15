9f0a794ef69eb7fd6ee1aa3fd385192122838c3f
baekyeon9 <dev.baekyeon@gmail.com>
2021-07-15 22:05:45 +0900

    .

--- java-basic/src/main/java/com/eomcs/lang/Hello3.java
+++ java-basic/src/main/java/com/eomcs/lang/Hello3.java
@@ -1,22 +1,30 @@
 package com.eomcs.lang;
 
+import java.util.Scanner;
+
 public class Hello3 {
 
   public static void main(String[] args) { 
 
-    int [] arr1 = new int [5];
+    int score = 0;
+    char grade = ' ';
 
+    System.out.println("점수를 입력하세요> ");
+    Scanner keyScan = new Scanner(System.in);
+    score = keyScan.nextInt();
 
-    int[] arr2;
-    arr2 = new int [4]; 
+    if (score >= 90) {
+      grade = 'A';
 
+    } else if (score >= 80) {
+      grade = 'B';
 
-    arr1[0] = 100;
-    arr1[1] = 200;
+    } else if (score >= 70) {
+      grade = 'C';
 
-
-    int [] arr3 = {10, 20, 30, 40, 50};
-
-
+    } else {
+      grade = 'D';
+    }  
+    System.out.println("당신의 학점은 "+ grade +" 입니다.");
   }
 }
