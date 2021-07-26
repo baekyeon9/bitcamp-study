23d805f9405bd021340adf7453fdaf6fccf3d294
baekyeon9 <dev.baekyeon@gmail.com>
2021-07-26 17:52:52 +0900

    ,

--- study-project/app/src/main/java/com/eomcs/util/Prompt.java
+++ study-project/app/src/main/java/com/eomcs/util/Prompt.java
@@ -5,23 +5,32 @@ import java.util.Scanner;
 
 public class Prompt {
 
-  static Scanner keyScan = new Scanner(System.in);
+  static Scanner keyboardScan = new Scanner(System.in);
 
+  // 메서드의 접근 범위를 설정하지 않으면 
+  // 기본 접근 범위는 같은 패키지 및 하위 클래스 만이 접근할 수 있다.
+  // => 다른 패키지에서도 접근할 수 있도록 하려면 public 으로 공개해야 한다.
   public static String inputString(String title) {
     System.out.print(title);
-    return keyScan.nextLine();
+    return keyboardScan.nextLine();
   }
 
   public static int inputInt(String title) {
-    System.out.print(title);
-    return Integer.parseInt(keyScan.nextLine());
+    return Integer.parseInt(inputString(title));
   }
 
   public static Date inputDate(String title) {
-    System.out.print(title);
-    return Date.valueOf(keyScan.nextLine());
+    return Date.valueOf(inputString(title));
   }
+
   public static void close() {
-    keyScan.close();
+    keyboardScan.close();
   }
 }
+
+
+
+
+
+
+
