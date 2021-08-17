3aea1df949a8a62633f3b0d17f227b877a8ac9e0
baekyeon9 <dev.baekyeon@gmail.com>
2021-08-17 18:41:34 +0900

    .

--- java-basic/src/main/java/com/eomcs/oop/ex09/f/Pen.java
+++ java-basic/src/main/java/com/eomcs/oop/ex09/f/Pen.java
@@ -1,8 +1,7 @@
 // 인터페이스 사용 예:
 package com.eomcs.oop.ex09.f;
 
-// 펜 사용 규칙을 정의하자!
+// 스마트폰의 사용 규칙을 정의하자!
 public interface Pen {
   void write(String text);
-
 }
--- java-basic/src/main/java/com/eomcs/oop/ex11/d/Exam0322.java
+++ java-basic/src/main/java/com/eomcs/oop/ex11/d/Exam0322.java
@@ -1,7 +1,7 @@
-// 인스턴스 메서드에 정의된 local class : 변수를 찾는 순서
+// local class에서 바깥 메서드의 로컬 변수 접근 II
 package com.eomcs.oop.ex11.d;
 
-class D2 {
+class D22 {
   int v1 = 1;
 
   void m1() {
@@ -16,7 +16,7 @@ class D2 {
         // this 를 생략하면 다음 순서로 변수를 찾는다.
         // 1) 로컬 변수
         // 2) 인스턴스 변수
-        // 3) 메서드의 선언된 로컬 변수
+        // 3) 메서드에 선언된 로컬 변수
         // 4) 바깥 클래스의 인스턴스 변수 또는 스태틱 변수
       }
     }
@@ -26,10 +26,10 @@ class D2 {
   }
 }
 
-public class Exam0320 {
+public class Exam0322 {
 
   public static void main(String[] args) {
-    D2 obj = new D2();
+    D22 obj = new D22();
     obj.m1();
   }
 
