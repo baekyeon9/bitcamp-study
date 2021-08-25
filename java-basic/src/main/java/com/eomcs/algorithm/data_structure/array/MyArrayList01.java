ba5cc9773f95edb94fb1e9845c1884ee208e0ebd
baekyeon9 <dev.baekyeon@gmail.com>
2021-08-25 13:57:07 +0900

    .

--- java-basic/src/main/java/com/eomcs/algorithm/data_structure/array/MyArrayList01.java
+++ java-basic/src/main/java/com/eomcs/algorithm/data_structure/array/MyArrayList01.java
@@ -1,5 +1,4 @@
 package com.eomcs.algorithm.data_structure.array;
-
 //테스트1 - MyArrayListTest
 //1) 인스턴스/객체 (의 주소)를 담을 레퍼런스 배열을 준비한다.
 //2) 인스턴스를 추가하는 add(Object) 메서드 정의한다.
@@ -15,24 +14,37 @@ package com.eomcs.algorithm.data_structure.array;
 // - 인덱스가 유효하지 않으면 예외를 발생시킨다.
 //9) get(int)으로 유효하지 않은 인덱스의 값을 꺼낼 때 예외를 발생시킨다.
 //10) remove()를 수행한 다음에 맨 끝에 남아 있는 주소를 null로 설정하여
-//인스턴스의 레퍼런스 카운트를 한 개 줄인다.
-//- 인덱스가 유효하지 않으면 예외를 발생시킨다.
+// 인스턴스의 레퍼런스 카운트를 한 개 줄인다.
+// - 인덱스가 유효하지 않으면 예외를 발생시킨다.
 //11) set()을 호출할 때 인덱스가 유효하지 않으면 예외를 발생시킨다.
 //
 //테스트3 - MyArrayListTest3
 //12) 여러 개의 목록을 동시에 관리할 수 있도록
 //  MyArrayList에 선언된 레퍼런스 배열을 스태틱 대신 인스턴스로 전환한다.
 //- 개별적으로 관리해야 할 데이터는 인스턴스 변수를 사용해야 한다.
-//
-//테스트3 - MyArrayListTest3
-//12) 여러 개의 목록을 동시에 관리할 수 있도록
-//  MyArrayList에 선언된 레퍼런스 배열을 스태틱 대신 인스턴스로 전환한다.
-//- 개별적으로 관리해야 할 데이터는 인스턴스 변수를 사용해야 한다.
 //13) 캡슐화 적용하여 공개할 멤버와 공개하지 말아야 할 멤버를 구분한다.
+//14) ArrayList 인스턴스를 생성할 때 배열의 초기 크기를 설정할 수 있도록 생성자를 추가한다.
+//15) ArrayList 인스턴스를 생성할 때 초기 크기를 지정하지 않고 생성할 수 있도록 기본 생성자를 추가한다.
+//16) 배열 크기를 지정할 때 기본 크기 보다 큰 값이 되도록 생성자를 변경한다.
+//17) 배열의 기본 크기를 직접 숫자로 지정하지 말고 상수를 사용하여 지정한다.
+//18) 배열의 크기를 늘릴 때 자바에서 제공하는 Arrays를 사용하여 처리한다.
 public class MyArrayList01 {
 
-  private Object[] elementData = new Object[5];
+  private static final int DEFAULT_CAPACITY = 5;
+  private Object[] elementData;
   private int size;
+
+  public MyArrayList01() {
+    elementData = new Object[DEFAULT_CAPACITY];
+  }
+
+  public MyArrayList01(int initialCapacity) {
+    if (initialCapacity < DEFAULT_CAPACITY) {
+      elementData = new Object[DEFAULT_CAPACITY];
+    } else {
+      elementData = new Object[initialCapacity];
+    }
+  }
 
   public boolean add(Object e) {
     if (size == elementData.length) {
@@ -44,11 +56,14 @@ public class MyArrayList01 {
 
   private void grow() {
     System.out.println("오호라! 배열을 늘리자.");
-    Object[] newArray = new Object[elementData.length + (elementData.length >> 1)];
-    for (int i = 0; i < elementData.length; i++) {
-      newArray[i] = elementData[i];
-    }
-    elementData = newArray;
+    System.out.println();
+
+    //  Object[] newArray = new Object[elementData.length + (elementData.length >> 1)];
+    //  for (int i = 0; i < elementData.length; i++) {
+    //    newArray[i] = elementData[i];
+    //  }
+    //  elementData = newArray;
+
   }
 
 
@@ -59,38 +74,38 @@ public class MyArrayList01 {
     if (index < 0 || index > size) {
       throw new ArrayIndexOutOfBoundsException("인덱스가 유효하지 않습니다.");
     }
-    for (int i = size; i > index; i--) {
-      elementData[i] = elementData[i-1];
+    for (int i = size; i > index ; i--) {
+      elementData[i] = elementData[i - 1];
     }
     elementData[index] = element;
     size++;
   }
 
   public Object get(int index) {
-    if(index < 0 || index >= size) {
+    if (index < 0 || index >= size) {
       throw new ArrayIndexOutOfBoundsException("인덱스가 유효하지 않습니다.");
-    }    
+    }
     return elementData[index];
   }
 
-  public Object set(int index, Object  element) {
-    if(index < 0 || index >= size) {
+  public Object set(int index, Object element) {
+    if (index < 0 || index >= size) {
       throw new ArrayIndexOutOfBoundsException("인덱스가 유효하지 않습니다.");
     }
-    Object prevElement = elementData[index];
+    Object old = elementData[index];
     elementData[index] = element;
-    return prevElement;
+    return old;
   }
 
   public Object remove(int index) {
-    if(index < 0 || index >= size) {
+    if (index < 0 || index >= size) {
       throw new ArrayIndexOutOfBoundsException("인덱스가 유효하지 않습니다.");
     }
 
     Object old = elementData[index];
 
-    for (int i = index; i < size -1; i++) {
-      elementData[i] = elementData[i-1];
+    for (int i = index; i < size - 1; i++) {
+      elementData[i] = elementData[i + 1];
     }
 
     size--;
@@ -104,4 +119,6 @@ public class MyArrayList01 {
   public int size() {
     return this.size;
   }
+}
+
 }
